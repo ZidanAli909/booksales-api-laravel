580001bf056c06b17d5038414c6093bb503fec9c
Ahmad Zidan Ali <ahmadzidanali909@gmail.com>
2025-10-16 20:38:02 +0700

    Readjusting table Author (to follow the brief)

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -14,19 +14,30 @@ class AuthorSeeder extends Seeder
     public function run(): void
     {
         Author::create([
-            'name' => 'Leila Chudori'
+            'name' => 'Leila Chudori',
+            'photo' => 'leila-chudori.jpg',
+            'bio' => 'Penulis, jurnalis, dan kritikus film terkemuka asal Indonesia. Ia dikenal karena karyanya yang sering kali mengangkat isu-isu sejarah dan politik Indonesia.'
         ]);
         Author::create([
-            'name' => 'Om Jero'
+            'name' => 'Om Jero',
+            'photo' => 'om-jero.jpg',
+            'bio' => 'Kreator dan penyusun buku bimbingan belajar "Wangsit" untuk SNBT (SBMPTN) yang sangat populer di kalangan pelajar SMA di Indonesia.'
         ]);
         Author::create([
-            'name' => 'Mark Manson'
+            'name' => 'Mark Manson',
+            'photo' => 'mark-manson.jpg',
+            'bio' => 'Penulis, narablog, dan veteran pengembangan diri asal Amerika yang dikenal karena pendekatan motivasi yang inkonvensional, pragmatis, dan blak-blakan.'
         ]);
         Author::create([
-            'name' => 'Ferdiriva Hamzah'
+            'name' => 'Ferdiriva Hamzah',
+            'photo' => 'ferdiriva-hamzah.jpg',
+            'bio' => 'Dokter spesialis mata (retina) sekaligus penulis novel yang dikenal lewat karya trilogi buku komedi populer, Cado-Cado (Catatan Dodol Calon Dokter).'
+
         ]);
         Author::create([
-            'name' => 'Tan Malaka'
+            'name' => 'Tan Malaka',
+            'photo' => 'tan-malaka.jpg',
+            'bio' => 'Seorang pejuang kemerdekaan, politikus, dan pemikir yang berperan besar dalam sejarah Indonesia.'
         ]);
     }
 }

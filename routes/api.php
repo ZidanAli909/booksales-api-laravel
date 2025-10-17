096597b5f2075e3f2b0a16d12f4f1e8f75d775f4
Ahmad Zidan Ali <ahmadzidanali909@gmail.com>
2025-10-17 19:36:36 +0700

    Replacing native route with resource route

--- routes/api.php
+++ routes/api.php
@@ -10,14 +10,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::post('/books', [BookController::class, 'store']);
-Route::post('/books/{id}', [BookController::class, 'update']); // Tidak bisa PUT, why?
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
+// Route::get('/books', [BookController::class, 'index']);
+// Route::get('/books/{id}', [BookController::class, 'show']);
+// Route::post('/books', [BookController::class, 'store']);
+// Route::post('/books/{id}', [BookController::class, 'update']); // Tidak bisa PUT, why?
+// Route::delete('/books/{id}', [BookController::class, 'destroy']);
 
-Route::get('/authors', [AuthorController::class, 'index']);
-Route::post('/authors', [AuthorController::class, 'store']);
+Route::apiResource('books', BookController::class);
 
-Route::get('/genres', [GenreController::class, 'index']);
-Route::post('/genres', [GenreController::class, 'store']);
+// Route::get('/authors', [AuthorController::class, 'index']);
+// Route::post('/authors', [AuthorController::class, 'store']);
+
+Route::apiResource('authors', AuthorController::class);
+
+// Route::get('/genres', [GenreController::class, 'index']);
+// Route::post('/genres', [GenreController::class, 'store']);
+
+Route::apiResource('genres', GenreController::class);
